a9f6b04004dccde92a0251295162fd910aa812f3
nyanboul <nyanboul@gmail.com>
2025-03-01 07:42:37 +0900

    Update _app.js

--- _app.js
+++ _app.js
@@ -1,12 +1,17 @@
 import { useEffect } from 'react'
+import { AuthWrapper } from '../components/AuthWrapper'
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
     console.log("SUPABASE URL:", process.env.NEXT_PUBLIC_SUPABASE_URL)
-    console.log("SUPABASE KEY:", process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY?.substring(0, 5) + "...")
+    console.log("SUPABASE KEY exists:", !!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
   }, [])
   
-  return <Component {...pageProps} />
+  return (
+    <AuthWrapper>
+      <Component {...pageProps} />
+    </AuthWrapper>
+  )
 }
 
 export default MyApp 
